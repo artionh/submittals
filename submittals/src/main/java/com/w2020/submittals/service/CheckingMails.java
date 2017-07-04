5195a1f1454778e3bac08afdb26a47fe407a6321
w2020 <w2020@ubuntu2>
2017-07-04 15:37:04 +0200

    email fetch fixes

--- submittals/src/main/java/com/w2020/submittals/service/CheckingMails.java
+++ submittals/src/main/java/com/w2020/submittals/service/CheckingMails.java
@@ -1,25 +1,24 @@
 package com.w2020.submittals.service;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import javax.mail.Address;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.NoSuchProviderException;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
-
 import org.springframework.stereotype.Component;
-
 import com.w2020.submittals.pojo.Email;
 
 @Component
 public class CheckingMails {
 
-	public  List<Email> getEmails() {
+	public static List<Email> getEmails() {
 
 		Properties properties = new Properties();
 		/*
@@ -34,38 +33,27 @@ public class CheckingMails {
 		 */
 
 		try {
+			properties.put("mail.store.protocol", "pop3");
 			properties.put("mail.pop3.host", "pop.gmail.com");
-			properties.put("mail.pop3.port", 995);
+			properties.put("mail.pop3.port", "995");
 			properties.put("mail.pop3.starttls.enable", "true");
 			Session emailSession = Session.getDefaultInstance(properties);
 
-			// create the POP3 store object and connect with the pop server
 			Store store = emailSession.getStore("pop3s");
-
 			store.connect("pop.gmail.com", "besnik.palluqi@gmail.com", "Darkmoon35");
-
-			// create the folder object and open it
 			Folder emailFolder = store.getFolder("INBOX");
 			emailFolder.open(Folder.READ_ONLY);
 
-			// retrieve the messages from the folder in an array and print it
 			Message[] messages = emailFolder.getMessages();
 			System.out.println("messages.length---" + messages.length);
 
 			List<Email> emailList = new ArrayList<Email>();
-
 			for (int i = 0, n = messages.length; i < n; i++) {
-				if(i==50){
-					break;
-				}
-				Email emails = new Email();
-				emails.setFrom(messages[i].getFrom()[0]);
-				emails.setSubject(messages[i].getSubject());
-				emails.setText(messages[i].getContent().toString());
-				emailList.add(emails);
+
+				Email email = getEnvelope(messages[i]);
+				emailList.add(email);
 			}
 
-			// close the store and folder objects
 			emailFolder.close(false);
 			store.close();
 			return emailList;
@@ -76,19 +64,84 @@ public class CheckingMails {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}	
 		return null;
 	}
 
-/*	public static void main(String[] args) {
-		getEmails();
+	public static Email getEnvelope(Part part) throws Exception {
+		Message message = (Message) part;
+		List<Address> from = new ArrayList<Address>();
+		List<Address> to = new ArrayList<Address>();
+		Email email = new Email();
+
+		if (part.isMimeType("multipart/*")) {
+			Multipart mp = (Multipart) part.getContent();
+			for (int i = 0; i < mp.getCount(); i++) {
+
+			}
+			// get Atachments
+		}
+
+		if (message.getFrom() != null)
+
+		{
+			for (int j = 0; j < message.getFrom().length; j++)
+				from.add(message.getFrom()[j]);
+		}
+
+		if (message.getRecipients(Message.RecipientType.TO) != null) {
+			for (int j = 0; j < message.getRecipients(Message.RecipientType.TO).length; j++)
+				to.add(message.getRecipients(Message.RecipientType.TO)[j]);
+		}
+
+		if (message.getSubject() != null) {
+			email.setSubject(message.getSubject());
+		}
+
+		if (part.getContent() != null) {
+			email.setText(part.getContent().toString());
+		}
+		
+	/*	List<File> attachments = new ArrayList<File>();
+		for (Message message : temp) {
+		    Multipart multipart = (Multipart) message.getContent();
+		    // System.out.println(multipart.getCount());
+
+		    for (int i = 0; i < multipart.getCount(); i++) {
+		        BodyPart bodyPart = multipart.getBodyPart(i);
+		        if(!Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition()) &&
+		               !StringUtils.isNotBlank(bodyPart.getFileName())) {
+		            continue; // dealing with attachments only
+		        } 
+		        InputStream is = bodyPart.getInputStream();
+		        File f = new File("/tmp/" + bodyPart.getFileName());
+		        FileOutputStream fos = new FileOutputStream(f);
+		        byte[] buf = new byte[4096];
+		        int bytesRead;
+		        while((bytesRead = is.read(buf))!=-1) {
+		            fos.write(buf, 0, bytesRead);
+		        }
+		        fos.close();
+		        attachments.add(f);
+		    }*/
+
+		email.setFrom(from);
+		email.setTo(to);
+
+		return email;
+
+	}
+
+	public static void main(String[] args) {
+
 		long startTime = System.currentTimeMillis();
 		long endTime = System.currentTimeMillis();
-		long executeTime = endTime - startTime;
-		System.out.println("Time: " + executeTime);
+
 		for (Email email : getEmails()) {
 			System.out.println(email.toString());
 		}
-	}*/
+		long executeTime = endTime - startTime;
+		System.out.println("Time: " + executeTime);
+	}
 
 }
